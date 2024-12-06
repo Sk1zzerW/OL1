8d3c75c2f68221b0e5158a747a50fe48a8d38367
olegk <kbryzhenyukpc@gmail.com>
2024-12-06 11:10:21 +0200

    Добавьте файлы проекта.

---
+++ OL1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    double amount;
+    std::cout << "Дробовое число ";
+    std::cin >> amount;
+
+    int hryvnias = amount;
+    int kopecks = round((amount - hryvnias) * 100);
+
+    std::cout << hryvnias << " грн " << kopecks << " коп" << std::endl;
+
+    return 0;
+}
